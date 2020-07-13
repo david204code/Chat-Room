d98e3fa7fd6e24e7c4d98755dc82348f22ccede2
David Wu <david204code@gmail.com>
2020-07-13 22:28:51 +0100

    rails generate model User username AND validates_uniqueness_of :username

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_212215) do
+ActiveRecord::Schema.define(version: 2020_07_13_212529) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
