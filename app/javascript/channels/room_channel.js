714de0562cc0a7d59a9174f81be258ba81174d53
David Wu <david204code@gmail.com>
2020-07-15 13:28:18 +0100

    The message data is connected and subcribing to all connected rooms/channel

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,32 +1,37 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 1}, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("connected.....")
-  },
+document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = room_element.getAttribute('data-room-id'); 
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log(data)
-
-    const element = document.getElementById('user-id');
-    const user_id = Number(element.getAttribute('data-user-id'));
-
-    let html;
-
-    if (user_id === data.message.user_id) {
-      html = data.mine
-    } else {
-      html = data.theirs
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id}, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log("connected to " + room_id)
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log(data)
+  
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+  
+      let html;
+  
+      if (user_id === data.message.user_id) {
+        html = data.mine
+      } else {
+        html = data.theirs
+      }
+  
+      // append the data/message to the view
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
     }
-
-    // append the data/message to the view
-    const messageContainer = document.getElementById('messages')
-    messageContainer.innerHTML = messageContainer.innerHTML + html
-  }
-});
+  });  
+})
